3b0a0f1af2d8453334a3fde9ffec8116282e09f0
LenWinkler <lenwinkler86@gmail.com>
2019-10-03 16:23:12 -0400

    created increaseStrikes handler

--- web-application-testing/src/App.js
+++ web-application-testing/src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import Count from "./components/count"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Baseball Stats</p>
+      <Count />
     </div>
   );
 }
--- web-application-testing/src/components/count.js
+++ web-application-testing/src/components/count.js
@@ -1,7 +1,71 @@
 import React from "react";
+import styled from "styled-components";
 
-const Count = () => {
+const Counts = styled.div`
+    display: flex;
+    justify-content: center;
+`
 
+const CountDisplay = styled.div`
+    margin: 1rem;
+`
+
+const CounterNum = styled.p`
+    font-size: 2rem;
+`
+
+const Button = styled.button`
+
+`
+
+class Count extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            strikes: 0,
+            balls: 0
+        }
+    };
+
+    increaseStrikes = () => {
+        const strikesOnState = this.state.strikes;
+        if (strikesOnState < 3) {
+            this.setState(() => {
+                return {strikes: strikesOnState +1} 
+            })
+        } else {
+            this.setState(() => {
+                return {strikes: 0,
+                        balls: 0
+                    }
+            })
+        }
+    }
+
+    
+
+    render() {
+        return (
+            <div className="count-card">
+                <Counts>
+                    <CountDisplay>
+                            <CounterNum>0</CounterNum>
+                            <p># of Strikes</p>
+                    </CountDisplay>
+                    <CountDisplay>
+                            <CounterNum>0</CounterNum>
+                            <p># of Balls</p>
+                    </CountDisplay>
+                </Counts>
+                <div className="buttons">
+                    <Button>strike</Button>
+                    <Button>ball</Button>
+                    <Button>foul</Button>
+                    <Button>hit</Button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Count;
